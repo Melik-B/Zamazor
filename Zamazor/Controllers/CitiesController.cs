7a1fb34aa78892f51b17ccc32279b07499bcd784
Melik <melikbaykal06@gmail.com>
2023-07-25 14:52:37 +0300

    Add project files.

---
+++ Zamazor/Controllers/CitiesController.cs
@@ -0,0 +1,30 @@
+﻿using Business.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Zamazor.Controllers
+{
+    public class CitiesController : Controller
+    {
+        private readonly ICityService _cityService;
+
+        public CitiesController(ICityService cityService)
+        {
+            _cityService = cityService;
+        }
+
+        public IActionResult GetCities(int countryId) // Cities/GetCities/1
+        {
+            var result = _cityService.List(countryId);
+            var model = result.Data;
+            return Json(model);
+        }
+
+        [HttpPost]
+        public IActionResult PostCities(int countryId)
+        {
+            var result = _cityService.List(countryId);
+            var model = result.Data;
+            return Json(model);
+        }
+    }
+}
---
+++ Zamazor/Controllers/StoresController.cs
@@ -0,0 +1,93 @@
+﻿using Business.Enums;
+using Business.Models;
+using Business.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Zamazor.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class StoresController : Controller
+    {
+        private readonly IStoreService _storeService;
+
+        public StoresController(IStoreService storeService)
+        {
+            _storeService = storeService;
+        }
+
+        // GET: Stores
+        public ActionResult Index()
+        {
+            var model = _storeService.Query().ToList();
+            return View(model);
+        }
+
+        // GET: Stores/Details/5
+        public ActionResult Details(int id)
+        {
+            var model = _storeService.Query().SingleOrDefault(m => m.Id == id);
+            return View(model);
+        }
+
+        // GET: Stores/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Stores/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(StoreModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = _storeService.Add(model);
+                if (result.IsSuccessful)
+                    return RedirectToAction(nameof(Index));
+                ModelState.AddModelError("", result.Message);
+            }
+            return View(model);
+        }
+
+        // GET: Stores/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var model = _storeService.Query().SingleOrDefault(m => m.Id == id);
+            return View(model);
+        }
+
+        // POST: Stores/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(StoreModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = _storeService.Update(model);
+                if (result.IsSuccessful)
+                    return RedirectToAction(nameof(Index));
+                ModelState.AddModelError("", result.Message);
+            }
+            return View(model);
+        }
+
+        // GET: Stores/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var model = _storeService.Query().SingleOrDefault(m => m.Id == id);
+            return View(model);
+        }
+
+        // POST: Stores/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            _storeService.Delete(id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
